71454913351a1e89650761083a125cab6f43430c
Alex Johnson <alex@tablelist.com>
2014-11-17 10:54:17 -0500

    Refactor data.js

--- js/data.js
+++ js/data.js
@@ -1,50 +1,48 @@
+/******************************
+          Data.js 
+ ******************************/
+
 // Convert Google Location Data's .kml (renamed .xml)
 // into a usable JSON format.
 
 function loadData(next){
 
-	console.log("\nLoading JSON ...");
+	loadXML(convertData);
 
-	/******************************
-	    Fetch Data from XML (KML)
-	******************************/
+	// Load data from XML (kml) format
+	function loadXML(next){
 
-	$.ajax({
-	    url: "/data/alex.xml",
-	    dataType:"xml"
-	}).done(function(xmlData){
-		convertData(xmlData);
-	});
+		console.log("\nLoading XML ...");
+
+		$.ajax({
+		    url: "/data/alex.xml",
+		    dataType:"xml"
+		}).done(function(xmlData){
+			convertData(xmlData);
+		});
+	}
 
 	// Convert data into usable format
 	function convertData(xmlData){
-		
-		// Convert XML to JSON
-		// https://github.com/stsvilik/Xml-to-JSON
-	    var jsonData = xml.xmlToJSON(xmlData);
-	    
-	    console.log("XML Converted to JSON");
 
+		console.log("\nXML Converted to JSON");
+		
+		// Convert XML to JSON. See: https://github.com/stsvilik/Xml-to-JSON
+	    var jsonData = xml.xmlToJSON(xmlData);
+
+	    // Extract location data from google location kml structure
 	    var data = jsonData.kml.Document.Placemark['gx:Track'];
-	    
+
 	    /******************************
 	       Convert into Usable Format
 	     ******************************/
-
-	    var acc = [];
-
-	    acc = _.zip(data.when, data["gx:coord"]);
-
-	    acc = _.map(acc, function(point, key){
+	    data = _.map(_.zip(data.when, data["gx:coord"]), function(point, key){
 	    	return {
 	    		"date"     : point[0].Text,
 	    		'location' : point[1].Text.split(" "),
 	    	};
 	    })
 
-	    // console.log('\n Resulting Formatted Data');
-	    // console.log(acc);
-
-	    next(acc)
+	    next(data); // Callback
 	}
 }
